8e99803248300f794d4a82181a74e17929dedf7b
Adetayo <adetayoadegbulu@gmail.com>
2023-09-07 07:44:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This prints with a puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
