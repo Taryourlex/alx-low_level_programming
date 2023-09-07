1243aaffca1bdc020c54f51dbe1138bf69b8a384
Adetayo <adetayoadegbulu@gmail.com>
2023-09-07 10:18:35 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * * main - Print a printf function
- * *Return: 0 (Success)
-*/
+ * * Return: 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
