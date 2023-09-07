2bf15c1f65d2f992176c7e37a8ff840d85088ac3
Adetayo <adetayoadegbulu@gmail.com>
2023-09-07 10:13:06 -0700

    5-printf.c.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * * main - A programme that use the printf function
- * * Return: 0 (Success)
+ * main - Print a printf function
+ * Return: 0 (Success)
  */
 int main(void)
 {
