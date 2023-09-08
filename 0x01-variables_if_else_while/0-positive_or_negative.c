11779012dcea8293e3255b1fbe6a0725de96c1d3
Adetayo <adetayoadegbulu@gmail.com>
2023-09-08 07:09:42 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,6 +6,7 @@
  *
  * Return: 0 on success
  */
+
 int main(void)
 {
 	int n;
@@ -13,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n < 0 )
+	if (n < 0)
 	{
 		printf("%d is %s\n", n, "negative");
 	}
