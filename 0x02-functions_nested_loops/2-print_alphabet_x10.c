838d2082f2ad3eb6d5636bd79d1f407537e52c6c
Adetayo <adetayoadegbulu@gmail.com>
2023-09-12 12:38:29 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Print x10 alphabet in lower case
+ * print_alphabet_x10 - Make alphabet x10
  * Return: void
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
+	int i;
+	char j;
 
-	char c;
-	int i = 0;
-
-	while (i <= 9)
+	for (i = 0 ; i < 10 ; i++)
 	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-	i++;
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
 	}
 }
