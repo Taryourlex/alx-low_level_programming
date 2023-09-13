12355f673d4b04cc9e298f0a1efe5519b3f4dd73
Adetayo <adetayoadegbulu@gmail.com>
2023-09-12 17:04:28 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that sums two number
+ * @a: function 1
+ * @b: function 2
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+
+	return (a + b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,6 @@
 void times_table(void)
 {
 	int i, j, result;
-
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
