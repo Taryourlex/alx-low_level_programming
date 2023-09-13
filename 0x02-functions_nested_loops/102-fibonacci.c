d8c157040f2b77b15080d1320b90fc9bccaf8b3e
Adetayo <adetayoadegbulu@gmail.com>
2023-09-13 04:41:12 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,31 +1,29 @@
 #include <stdio.h>
 /**
- * main - first 50 fibonacci number
+ * main - Entry point
  *
  * Return: (0)
  */
 int main(void)
 {
-	int i;
-	long int fibonacci[50];
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
-	printf("%1d, %1d, ", fibonacci[0], fibonacci[1]);
+	int i = 0;
+	long j = 1, k = 2;
 
-	for (i = 2; i < 50; i++)
+	while (i < 50)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[1 - 2]
-
-		if (i == 49)
-		{
-			printf("%1d\n", fibonacci[i]);
-		}
-		else
-		{
-			printf("%1d, ", fibonacci[i]);
-		}
+	if (i == 0)
+	printf("%ld", j);
+	else if (i == 1)
+	printf(", %ld", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf(", %ld", k);
 	}
-
+	++i;
+	}
+	printf("\n");
 	return (0);
 }
