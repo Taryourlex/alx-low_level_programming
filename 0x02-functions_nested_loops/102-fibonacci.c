db27ad56da15646fa5f57c69017e1a0911ea165e
Adetayo <adetayoadegbulu@gmail.com>
2023-09-13 04:06:34 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - first 50 fibonacci number
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i;
+	long int fibonacci[50];
+
+	fibonacci[0] = 1;
+	fibonacci[1] = 2;
+	printf("%1d, %1d, ", fibonacci[0], fibonacci[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		fibonacci[i] = fibonacci[i - 1] + fibonacci[1 - 2]
+
+		if (i == 49)
+		{
+			printf("%1d\n", fibonacci[i]);
+		}
+		else
+		{
+			printf("%1d, ", fibonacci[i]);
+		}
+	}
+
+	return (0);
+}
