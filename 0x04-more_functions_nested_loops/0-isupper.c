219a9e24a0809650cf0d4b6d7dea082ac14450d5
Adetayo <adetayoadegbulu@gmail.com>
2023-09-14 06:50:41 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: The functions to be checked
+ * Return: 1 if it is uppercase or 0 if otherwise
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int mul - multiply two integers
+ * @a: The first integer
+ * @b: The second one
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return (result);
+}
